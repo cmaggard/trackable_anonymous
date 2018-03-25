70725ffabae6715457fde8cc1f57d12548332132
Cody Maggard <cody.maggard@gmail.com>
2018-03-25 04:51:32 -0400

    Main commit

---
+++ lib/trackable_anonymous/plug.ex
@@ -0,0 +1,39 @@
+defmodule TrackableAnonymous.Plug do
+  import Plug.Conn
+  alias Plug.Conn
+
+  @behaviour Plug
+
+  def init(opts) do
+    Keyword.get(opts, :session_key, :trackable_id)
+  end
+
+  def call(conn, session_key) do
+    conn
+    |> get_unique_id(session_key)
+    |> set_unique_id(session_key)
+  end
+
+  def get_unique_id(conn, session_key) do
+    case Conn.get_session(conn, session_key) do
+      nil -> {conn, generate_id()}
+      val -> {conn, val}
+    end
+  end
+
+  def set_unique_id({conn, key_value}, session_key) do
+    conn
+    |> Conn.put_session(session_key, key_value)
+    |> Conn.assign(session_key, key_value)
+  end
+
+  def generate_id do
+    binary = <<
+      System.system_time(:nanoseconds)::64,
+      :erlang.phash2({node(), self()}, 16_777_216)::24,
+      :erlang.unique_integer()::32
+    >>
+
+    Base.hex_encode32(binary, case: :lower)
+  end
+end
